05778491b76c9080bec795678335045cb452cfad
Ignacio Puppo <33910974+Archcio@users.noreply.github.com>
2020-03-04 01:06:42 -0300

    Add files via upload

---
+++ precache-manifest.6157cfaf3ff895c93c4a414cc8594274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0faa8ec99a584ac75530db2576a0e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b43e4049980a502f82",
+    "url": "/static/css/main.0f0689f8.chunk.css"
+  },
+  {
+    "revision": "1d1c3e6d4d4f40832a47",
+    "url": "/static/js/2.ff72a521.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ff72a521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b43e4049980a502f82",
+    "url": "/static/js/main.83dece0e.chunk.js"
+  },
+  {
+    "revision": "a21303dc03072cd36419",
+    "url": "/static/js/runtime-main.9d19e97d.js"
+  }
+]);
